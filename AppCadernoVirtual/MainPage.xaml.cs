80451b7682e45554ee275508c20e34164340fc12
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-05-08 13:13:51 -0300

    comecei a fazer os comentários do código

--- AppCadernoVirtual/MainPage.xaml.cs
+++ AppCadernoVirtual/MainPage.xaml.cs
@@ -8,18 +8,22 @@ namespace AppCadernoVirtual
         public MainPage()
         {
             InitializeComponent();
+            //O BtnPrimeiro.Source armazenará a imagem do botão
             BtnPrimeiro.Source = ImageSource.FromResource("AppCadernoVirtual.Imagens.primeiro.png");
             BtnSegundo.Source = ImageSource.FromResource("AppCadernoVirtual.Imagens.segundo.png");
             BtnTerceiro.Source = ImageSource.FromResource("AppCadernoVirtual.Imagens.terceiro.png");
 
+            //Esta instrução tirará a barra de navegação
             NavigationPage.SetHasNavigationBar(this, false);
         }
         private void Escolha1_Clicked(object sender, EventArgs e)
         {
+            //Aqui o botão executará o comando try que irá navegar para a tela inicial ao ser clicado
             try
             {
                 Navigation.PushAsync(new TelaInicial());
             }
+            // O catch é o se não. Se não der certo o comando acima dará um erro
             catch (Exception ex)
             {
                 DisplayAlert("Ops!", ex.Message, "Ok :(");
